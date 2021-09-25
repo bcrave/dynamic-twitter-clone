ddca11c00e916c802ae58f9f6782cca79a8b6cd9
Brendon Crave <brendoncrave@devpipeline.com>
2021-09-25 19:05:32 +0400

    fully dynamic page

--- index.js
+++ index.js
@@ -49,17 +49,29 @@ const user2 = {
     {
       text: "Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/",
       timestamp: "2/10/2021 00:01:20",
+      commentCount: 4200,
+      retweetCount: 6900,
+      likeCount: 66600,
     },
     {
       text: "Should I start tweeting memes? Let me know in a comment.",
       timestamp: "2/09/2021 18:37:12",
+      commentCount: 5800,
+      retweetCount: 8900,
+      likeCount: 74700,
     },
     {
       text: "In 2020, I read a book every hour.",
       timestamp: "2/09/2021 12:11:51",
+      commentCount: 7200,
+      retweetCount: 1080,
+      likeCount: 69000,
     },
   ],
 };
+
+const userParams = new URLSearchParams(window.location.search);
+const user = userParams.get("user");
 
 const headerDisplay = document.getElementById("header-display");
 const profileImages = document.querySelector(".profile-images");
@@ -70,35 +82,49 @@ const tweet = document.querySelector(".tweet");
 /* Templating */
 headerDisplay.innerHTML = `
   <h2 class="display-name">
-    ${user1.displayName}
+    ${user === "user1" ? user1.displayName : user2.displayName}
     <span>
       <i class="fas fa-certificate"></i>
     </span>
   </h2>
-  <p class="sub-info">${user1.tweetCount / 1000}K Tweets</p>
+  <p class="sub-info">${
+    user === "user1" ? user1.tweetCount / 1000 : user2.tweetCount / 1000
+  }K Tweets</p>
 `;
-profileImages.style.backgroundImage = `url(${user1.coverPhotoURL})`;
-profileImages.innerHTML = `<img src="${user1.avatarURL}" alt="${user1.displayName}" />`;
+profileImages.style.backgroundImage = `url(${
+  user === "user1" ? user1.coverPhotoURL : user2.coverPhotoURL
+})`;
+profileImages.innerHTML = `<img src="${
+  user === "user1" ? user1.avatarURL : user2.avatarURL
+}" alt="${user == "user1" ? user1.displayName : user2.displayName}" />`;
 profileDisplay.innerHTML = `
   <h2 class="display-name">
-    ${user1.displayName}
+    ${user === "user1" ? user1.displayName : user2.displayName}
     <span>
       <i class="fas fa-certificate"></i>
     </span>
   </h2>
   <div class="sub-info">
     <div>
-      <p class="username">${user1.userName}</p>
+      <p class="username">${
+        user === "user1" ? user1.userName : user2.userName
+      }</p>
     </div>
     <div class="join-date">
       <i class="far fa-calendar-alt"></i>
-      <p>Joined ${user1.joinedDate}</p>
+      <p>Joined ${user === "user1" ? user1.joinedDate : user2.joinedDate}</p>
     </div>
     <div class="follower-info">
       <p>
-        <span class="following">${user1.followingCount}</span>
+        <span class="following">${
+          user === "user1" ? user1.followingCount : user2.followingCount
+        }</span>
         Following
-        <span class="followers">${user1.followerCount / 1000000}M</span>
+        <span class="followers">${
+          user === "user1"
+            ? user1.followerCount / 1000000
+            : user2.followerCount / 1000000
+        }M</span>
         Followers
       </p>
     </div>
@@ -110,34 +136,54 @@ for (let i = 0; i < user1.tweets.length; i++) {
   tweet.classList.add("tweet");
   tweet.innerHTML = `
     <div class="tweet-image">
-      <img src="${user1.avatarURL}" alt="${user1.displayName}" />
+      <img src="${user === "user1" ? user1.avatarURL : user2.avatarURL}" alt="${
+    user === "user1" ? user1.displayName : user2.displayName
+  }" />
     </div>
     <div class="tweet-content">
       <div class="tweet-info">
         <div class="tweet-info-left">
-          <h3>${user1.displayName}</h3>
+          <h3>${user === "user1" ? user1.displayName : user2.displayName}</h3>
           <i class="fas fa-certificate"></i>
-          <p class="username">${user1.userName}</p>
+          <p class="username">${
+            user === "user1" ? user1.userName : user2.userName
+          }</p>
           &middot;
-          <p>${user1.tweets[i].timestamp.slice(0, 9)}</p>
+          <p>${
+            user === "user1"
+              ? user1.tweets[i].timestamp.slice(0, 9)
+              : user2.tweets[i].timestamp.slice(0, 9)
+          }</p>
         </div>
         <div class="tweet-info-right">&bull; &bull; &bull;</div>
       </div>
       <div class="tweet-message">
-        <p>${user1.tweets[i].text}</p>
+        <p>${user === "user1" ? user1.tweets[i].text : user2.tweets[i].text}</p>
       </div>
       <ul class="tweet-actions">
         <li>
           <i class="far fa-comment"></i>
-          <p>${user1.tweets[i].commentCount / 1000}K</p>
+          <p>${
+            user === "user1"
+              ? user1.tweets[i].commentCount / 1000
+              : user2.tweets[i].commentCount / 1000
+          }K</p>
         </li>
         <li>
           <i class="fas fa-retweet"></i>
-          <p>${user1.tweets[i].retweetCount / 1000}K</p>
+          <p>${
+            user === "user1"
+              ? user1.tweets[i].retweetCount / 1000
+              : user2.tweets[i].retweetCount / 1000
+          }K</p>
         </li>
         <li>
           <i class="far fa-heart"></i>
-          <p>${user1.tweets[i].likeCount / 1000}K</p>
+          <p>${
+            user === "user1"
+              ? user1.tweets[i].likeCount / 1000
+              : user2.tweets[i].likeCount / 1000
+          }K</p>
         </li>
         <li><i class="fas fa-share-square"></i></li>
       </ul>
